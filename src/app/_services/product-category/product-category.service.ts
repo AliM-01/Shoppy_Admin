3f96c29031e7e1c286b81c41caf0cfc57edd5ee2
Ali M <alimousavi-01@outlook.com>
2021-10-24 21:47:44 +0330

    Initialized ProductCategoryService, Implemented filterProductCategory action, and fixed path

---
+++ src/app/_services/product-category/product-category.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IResponse } from 'src/app/_models/common/IResponse';
+import { environment } from '@environments/environment';
+import { FilterProductCategory } from '@app_models/product-category/_index';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductCategoryService {
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  filterProductCategory(filter: FilterProductCategory): Observable<IResponse<FilterProductCategory>> {
+
+    let params;
+
+    if (filter !== null) {
+      params = new HttpParams()
+        .set('pageId', filter.pageId.toString())
+        .set('title', filter.title)
+    }
+
+    return this.http.get<IResponse<FilterProductCategory>>(`${environment.apiUrl}/product-category/get-sliders`, { params });
+  }
+}
