6b64fc180b69eed11eaf4c99cbf59fc1e04e89e5
Ali M <alimousavi-01@outlook.com>
2021-11-09 20:30:03 +0330

    Initialized CKEditor Component

--- src/app/_components/components.module.ts
+++ src/app/_components/components.module.ts
@@ -5,23 +5,28 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { PreloaderComponent } from './preloader/preloader.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { CkeditorComponent } from './ckeditor/ckeditor.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     SidebarComponent,
     FooterComponent,
-    PreloaderComponent
+    PreloaderComponent,
+    CkeditorComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    CKEditorModule
   ],
   exports: [
     HeaderComponent,
     SidebarComponent,
     FooterComponent,
-    PreloaderComponent
+    PreloaderComponent,
+    CkeditorComponent
   ],
   schemas: []
 })
