44378a2c10e8f66836cd89ba4e4c1fc465ac6833
Ali M <alimousavi-01@outlook.com>
2021-11-12 13:03:25 +0330

    Implemented createProduct Service

--- src/app/_services/product/product.service.ts
+++ src/app/_services/product/product.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { IResponse } from '@app_models/common/IResponse';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
-import { EditProductModel, FilterProductModel } from '../../_models/product/_index';
+import { CreateProductModel, EditProductModel, FilterProductModel } from '../../_models/product/_index';
 
 @Injectable({
   providedIn: 'root'
@@ -27,5 +27,26 @@ export class ProductService {
   
   getProductDetails(id: number): Observable<IResponse<EditProductModel>> {
     return this.http.get<IResponse<EditProductModel>>(`${environment.apiUrl}/product/${id}`);
+  }
+
+  
+  createProduct(createData: CreateProductModel):Observable<IResponse<any>> {
+    
+    const formData = new FormData();
+
+    formData.append('categoryId', createData.categoryId.toString());
+    formData.append('title', createData.title);
+    formData.append('code', createData.code);
+    formData.append('unitPrice', createData.unitPrice);
+    formData.append('isInStock', createData.isInStock.valueOf.toString());
+    formData.append('shortDescription', createData.shortDescription);
+    formData.append('description', createData.description);
+    formData.append('imageFile', createData.imageFile, createData.imageFile.name);
+    formData.append('imageAlt', createData.imageAlt);
+    formData.append('imageTitle', createData.imageTitle);
+    formData.append('metaKeywords', createData.metaKeywords);
+    formData.append('metaDescription', createData.metaDescription);
+    
+    return this.http.post<IResponse<any>>(`${environment.apiUrl}/product-/create-product`, formData);
   }
 }
