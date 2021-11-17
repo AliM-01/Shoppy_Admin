3e2b28acbce60c65824a04ddeeb3c9bce8badd53
Ali M <alimousavi-01@outlook.com>
2021-11-18 02:48:15 +0330

    Implemented createProductPicture In Service

--- src/app/_services/product-picture/product-category.service.ts
+++ src/app/_services/product-picture/product-category.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IResponse } from 'src/app/_models/common/IResponse';
 import { environment } from '@environments/environment';
-import { FilterProductPictureModel, EditProductPictureModel } from '@app_models/product-picture/_index';
+import { FilterProductPictureModel, EditProductPictureModel, CreateProductPictureModel } from '@app_models/product-picture/_index';
 @Injectable({
   providedIn: 'root'
 })
@@ -28,5 +28,17 @@ export class ProductPictureService {
 
   getProductPictureDetails(id: number): Observable<IResponse<EditProductPictureModel>> {
     return this.http.get<IResponse<EditProductPictureModel>>(`${environment.apiUrl}/product-picture/${id}`);
+  }
+
+  createProductPicture(createData: CreateProductPictureModel):Observable<IResponse<any>> {
+    
+    const formData = new FormData();
+    
+    formData.append('productId', createData.productId.toString());
+    formData.append('imageFile', createData.imageFile, createData.imageFile.name);
+    formData.append('imageAlt', createData.imageAlt);
+    formData.append('imageTitle', createData.imageTitle);
+    
+    return this.http.post<IResponse<any>>(`${environment.apiUrl}/product-Picture/create-product-Picture`, formData);
   }
 }
