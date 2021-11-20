a9c6584d5ac3729cba62cf1331326ad6061bb281
Ali M <alimousavi-01@outlook.com>
2021-11-20 19:04:12 +0330

    Created SliderDataSource

---
+++ src/app/_models/slider/slider-data-source.ts
@@ -0,0 +1,48 @@
+import { CollectionViewer, DataSource } from "@angular/cdk/collections";
+import { SliderService } from "@app_services/slider/slider.service";
+import { BehaviorSubject, Observable, of } from "rxjs";
+import { SliderModel} from "./_index";
+import { catchError, finalize } from 'rxjs/operators';
+import { IResponse } from '../common/IResponse';
+
+export class SliderDataSource implements DataSource<SliderModel> {
+
+    private slidersSubject = new BehaviorSubject<SliderModel[]>([]);
+    private loadingSubject = new BehaviorSubject<boolean>(true);
+
+    public loading$ = this.loadingSubject.asObservable();
+    public length : number = 0;
+
+    constructor(private sliderService: SliderService) {}
+
+    connect(collectionViewer: CollectionViewer=null): Observable<SliderModel[]> {
+        return this.slidersSubject.asObservable();
+    }
+
+    disconnect(collectionViewer: CollectionViewer=null): void {
+        this.slidersSubject.complete();
+        this.loadingSubject.complete();
+    }
+
+    loadSliders() {
+
+        this.loadingSubject.next(true);
+
+        this.sliderService.getSlidersList()
+        .pipe(catchError(() => of([])),finalize(() => this.loadingSubject.next(true)))
+        .subscribe((res : IResponse<any>) => {
+            
+
+            setInterval(() => {
+                
+                this.length = res.data.sliders.length;
+
+                this.slidersSubject.next(res.data.Sliders);
+                
+                this.loadingSubject.next(false);
+            }, 500)
+        });
+
+    }    
+}
+
