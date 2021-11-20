ad4045bb925936b1b638887a2fd85080edabc33e
Ali M <alimousavi-01@outlook.com>
2021-11-20 18:53:01 +0330

    Implemented getSlidersList Service

--- src/app/_services/slider/slider.service.ts
+++ src/app/_services/slider/slider.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IResponse } from '@app_models/common/IResponse';
+import { SliderModel } from '@app_models/slider/slider';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,5 +12,7 @@ export class SliderService {
     private http: HttpClient
   ) { }
 
-    
+  getSlidersList(): Observable<IResponse<SliderModel[]>> {
+    return this.http.get<IResponse<SliderModel[]>>(`${environment.apiUrl}/slider/get-list`);
+  }
 }
