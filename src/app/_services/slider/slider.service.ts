dbfa96d8f38faf97d74cb82c07f7af40d0e1ae64
Ali M <alimousavi-01@outlook.com>
2021-11-20 18:56:19 +0330

    Implemented getSliderDetails Service

--- src/app/_services/slider/slider.service.ts
+++ src/app/_services/slider/slider.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IResponse } from '@app_models/common/IResponse';
-import { SliderModel } from '@app_models/slider/slider';
+import { SliderModel, EditSliderModel, CreateSliderModel } from '@app_models/slider/_index';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
 @Injectable({
@@ -15,4 +15,23 @@ export class SliderService {
   getSlidersList(): Observable<IResponse<SliderModel[]>> {
     return this.http.get<IResponse<SliderModel[]>>(`${environment.apiUrl}/slider/get-list`);
   }
+
+  getSliderDetails(id: number): Observable<IResponse<EditSliderModel>> {
+    return this.http.get<IResponse<EditSliderModel>>(`${environment.apiUrl}/slider/${id}`);
+  }
+
+  // createSlider(createData: CreateSliderModel):Observable<IResponse<any>> {
+    
+  //   const formData = new FormData();
+    
+  //   formData.append('heading', createData.heading);
+  //   formData.append('text', createData.text);
+  //   formData.append('imageFile', createData.imageFile, createData.imageFile.name);
+  //   formData.append('imageAlt', createData.imageAlt);
+  //   formData.append('imageTitle', createData.imageTitle);
+  //   formData.append('btnLink', createData.btnLink);
+  //   formData.append('btnText', createData.btnText);
+    
+  //   return this.http.post<IResponse<any>>(`${environment.apiUrl}/slider/create-slider`, formData);
+  // }
 }
