ff40824edcc53b2a9e03ab388f865b82be89ba9b
Ali M <alimousavi-01@outlook.com>
2021-12-01 18:53:45 +0330

    Fully Implemented CustomerDiscountService

---
+++ src/app/_services/discount/customer-discount/customer-discount.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IResponse } from '@app/_models/common/IResponse';
+import { environment } from '@environments/environment';
+import { CreateCustomerDiscountModel, EditCustomerDiscountModel, FilterCustomerDiscountModel } from '@app_models/discount/customer-discount/_index';
+
+@Injectable({
+  providedIn: 'platform'
+})
+export class CustomerDiscountService {
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  filterCustomerDiscount(filter: FilterCustomerDiscountModel): Observable<IResponse<FilterCustomerDiscountModel>> {
+
+    let params;
+
+    if (filter !== null) {
+      params = new HttpParams()
+        .set('productId', filter.productId.toString())
+    }
+
+    return this.http.get<IResponse<FilterCustomerDiscountModel>>(`${environment.shopBaseApiUrl}/customer-discount/filter-product-categories`, { params });
+  }
+
+  getCustomerDiscountDetails(id: number): Observable<IResponse<EditCustomerDiscountModel>> {
+    return this.http.get<IResponse<EditCustomerDiscountModel>>(`${environment.shopBaseApiUrl}/customer-discount/${id}`);
+  }
+
+  createCustomerDiscount(createData: CreateCustomerDiscountModel):Observable<IResponse<any>> {
+    
+    const formData = new FormData();
+    
+    formData.append('rate', createData.rate.toString());
+    formData.append('description', createData.description);
+    
+    return this.http.post<IResponse<any>>(`${environment.shopBaseApiUrl}/customer-discount/create-customer-discount`, formData);
+  }
+
+  editCustomerDiscount(editData: EditCustomerDiscountModel):Observable<IResponse<any>> {
+    const formData = new FormData();
+
+    formData.append('id', editData.id.toString());
+    formData.append('rate', editData.rate.toString());
+    formData.append('description', editData.description);
+    
+    return this.http.put<IResponse<any>>(`${environment.shopBaseApiUrl}/customer-discount/edit-customer-discount`, formData);
+  }
+
+  deleteCustomerDiscount(CustomerDiscountId: number):Observable<IResponse<any>> {
+    return this.http.delete<IResponse<any>>(`${environment.shopBaseApiUrl}/customer-discount/delete-customer-discount/${CustomerDiscountId}`);
+  }
+
+}
