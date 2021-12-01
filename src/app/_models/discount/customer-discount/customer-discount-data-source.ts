a6da1669adf9334ba28052c70ed78e12b559b6d0
Ali M <alimousavi-01@outlook.com>
2021-12-01 18:56:20 +0330

    Created CustomerDiscountDataSource

---
+++ src/app/_models/discount/customer-discount/customer-discount-data-source.ts
@@ -0,0 +1,48 @@
+import { CollectionViewer, DataSource } from "@angular/cdk/collections";
+import { BehaviorSubject, Observable, of } from "rxjs";
+import { FilterCustomerDiscountModel, CustomerDiscountModel} from "./_index";
+import { catchError, finalize } from 'rxjs/operators';
+import { IResponse } from '@app_models/common/IResponse';
+import { CustomerDiscountService } from "@app_services/discount/customer-discount/customer-discount.service";
+
+export class CustomerDiscountDataSource implements DataSource<CustomerDiscountModel> {
+
+    private customerDiscountsSubject = new BehaviorSubject<CustomerDiscountModel[]>([]);
+    private loadingSubject = new BehaviorSubject<boolean>(true);
+
+    public loading$ = this.loadingSubject.asObservable();
+    public length : number = 0;
+
+    constructor(private customerDiscountService: CustomerDiscountService) {}
+
+    connect(collectionViewer: CollectionViewer=null): Observable<CustomerDiscountModel[]> {
+        return this.customerDiscountsSubject.asObservable();
+    }
+
+    disconnect(collectionViewer: CollectionViewer=null): void {
+        this.customerDiscountsSubject.complete();
+        this.loadingSubject.complete();
+    }
+
+    loadCustomerDiscounts(filterCustomerDiscounts: FilterCustomerDiscountModel) {
+
+        this.loadingSubject.next(true);
+
+        this.customerDiscountService.filterCustomerDiscount(filterCustomerDiscounts)
+        .pipe(catchError(() => of([])),finalize(() => this.loadingSubject.next(true)))
+        .subscribe((res : IResponse<any>) => {
+            
+
+            setInterval(() => {
+                
+                this.length = res.data.discounts.length;
+
+                this.customerDiscountsSubject.next(res.data.CustomerDiscounts);
+                
+                this.loadingSubject.next(false);
+            }, 500)
+        });
+
+    }    
+}
+
