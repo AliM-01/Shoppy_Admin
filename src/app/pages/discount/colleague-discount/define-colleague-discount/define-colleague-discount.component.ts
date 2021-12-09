9733a00b68a6a54b66b3b0ed1cccf83ff6dcbc2f
Ali M <alimousavi-01@outlook.com>
2021-12-09 20:07:14 +0330

    Refactor checkProductHasColleagueDiscount Feature

--- src/app/pages/discount/colleague-discount/define-colleague-discount/define-colleague-discount.component.ts
+++ src/app/pages/discount/colleague-discount/define-colleague-discount/define-colleague-discount.component.ts
@@ -1,19 +1,22 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { DefineColleagueDiscountModel } from '@app_models/discount/colleague-discount/define-colleague-discount';
 import { ColleagueDiscountService } from '@app_services/discount/colleague-discount/colleague-discount.service';
 import { ToastrService } from 'ngx-toastr';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-define-colleague-discount',
   templateUrl: './define-colleague-discount.component.html'
 })
-export class DefineColleagueDiscountComponent implements OnInit {
+export class DefineColleagueDiscountComponent implements OnInit, AfterViewInit {
 
   defineForm: FormGroup;
   existsProductDiscount: boolean = false;
+  @ViewChild('productIdInput') productIdInput: ElementRef;
 
   constructor(
     public dialogRef: MatDialogRef<DefineColleagueDiscountComponent>,
@@ -30,20 +33,26 @@ export class DefineColleagueDiscountComponent implements OnInit {
 
   }
 
+  ngAfterViewInit() {
+
+    fromEvent(this.productIdInput.nativeElement, 'keyup')
+      .pipe(
+        debounceTime(150),
+        distinctUntilChanged(),
+        tap(() => {
+          this.checkProductHasColleagueDiscount();
+        })
+      )
+      .subscribe();
+  }
+
   onCloseClick(): void {
     this.dialogRef.close();
   }
 
   submitDefineForm() {
 
-    this.colleagueDiscountService.checkProductHasColleagueDiscount(this.defineForm.controls.productId.value).subscribe(res => {
-      
-      if(res.data.existsColleagueDiscount === true){
-        this.toastr.info("برای این محصول یک تخفیف فعال وجود دارد", "اطلاعات");
-        this.existsProductDiscount = true
-      }
-
-    });
+    this.checkProductHasColleagueDiscount();
 
     if(!this.existsProductDiscount){
 
@@ -91,5 +100,16 @@ export class DefineColleagueDiscountComponent implements OnInit {
 
     
 
+  }
+
+  checkProductHasColleagueDiscount(){
+    this.colleagueDiscountService.checkProductHasColleagueDiscount(this.defineForm.controls.productId.value).subscribe(res => {
+      
+      if(res.data.existsColleagueDiscount === true){
+        this.toastr.info("برای این محصول یک تخفیف فعال وجود دارد", "اطلاعات");
+        this.existsProductDiscount = true
+      }
+
+    });
   }
 }
