d207148343698f2fa34480e8be1fa0551915a1c9
Ali M <alimousavi-01@outlook.com>
2021-12-12 20:53:29 +0330

    Added inventoryBaseApiUrl to Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   shopBaseApiUrl: "https://localhost:5001/api/admin/shop",
   discountBaseApiUrl: "https://localhost:5001/api/admin/discount",
+  inventoryBaseApiUrl: "https://localhost:5001/api/admin/inventory",
   productCategoryBaseImagePath: "https://localhost:5001/product_category",
   productBaseImagePath: "https://localhost:5001/product",
   productPicutreBaseImagePath: "https://localhost:5001/product_picture",
