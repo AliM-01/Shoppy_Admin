24c1b714bd97fbbdc754e43c93c070f6cc85b258
Ali M <alimousavi-01@outlook.com>
2021-12-12 20:59:07 +0330

    Implemented filterInventory Service

--- src/app/_services/inventory/inventory.service.ts
+++ src/app/_services/inventory/inventory.service.ts
@@ -1,5 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IResponse } from '@app_models/common/IResponse';
+import { FilterInventoryModel } from '@app_models/inventory/_index';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'platform'
@@ -8,5 +12,18 @@ export class InventoryService {
   constructor(
     private http: HttpClient
   ) { }
+
+  filterInventory(filter: FilterInventoryModel): Observable<IResponse<FilterInventoryModel>> {
+
+    let params;
+
+    if (filter !== null) {
+      params = new HttpParams()
+        .set('productId', filter.productId.toString())
+        .set('inStock', (filter.inStock === true ? 'true' : 'false'));
+    }
+
+    return this.http.get<IResponse<FilterInventoryModel>>(`${environment.inventoryBaseApiUrl}/filter`, { params });
+  }
 
 }
