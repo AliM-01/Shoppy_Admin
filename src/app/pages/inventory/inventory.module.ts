8e8ddd4e53907191d1743752f81f9394bb318f7e
Ali M <alimousavi-01@outlook.com>
2021-12-13 17:54:29 +0330

    Initialized InventoryModule

---
+++ src/app/pages/inventory/inventory.module.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '@app_components/components.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
+import { DirectivesModule } from '@app_directives/directives.module';
+import { InventoryService } from '@app_services/inventory/inventory.service';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    ComponentsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    DirectivesModule
+  ],
+  exports: [],
+  schemas: [],
+  providers: [InventoryService]
+})
+export class InventoryModule { }
