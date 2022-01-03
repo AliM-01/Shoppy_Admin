141711b4b13a8b8aa2b445e41bee44154f0122aa
Ali M <alimousavi-01@outlook.com>
2022-01-03 18:37:22 +0330

    Implemented loadingOn & loadingOff

--- src/app/_services/common/loading/loading.service.ts
+++ src/app/_services/common/loading/loading.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class LoadingService {
 
-  loading$: Observable<boolean>;
+  private loadingSubject:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  loading$: Observable<boolean> = this.loadingSubject.asObservable();
 
+  showLoaderUntilCompleted<T>(obs: Observable<T>): Observable<T>{
+    return undefined;
+  }
+
+  loadingOn(){
+    this.loadingSubject.next(true);
+  }
+
+  
+  loadingOff(){
+    this.loadingSubject.next(false);
+  }
+
+  
 }
