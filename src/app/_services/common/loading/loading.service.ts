92f845efb35363cb4db60fe5153e876153771be7
Ali M <alimousavi-01@outlook.com>
2022-01-03 22:00:27 +0330

    Code Cleanup

--- src/app/_services/common/loading/loading.service.ts
+++ src/app/_services/common/loading/loading.service.ts
@@ -8,18 +8,12 @@ export class LoadingService {
 
   loading$: Observable<boolean> = this.loadingSubject.asObservable();
 
-  showLoaderUntilCompleted<T>(obs: Observable<T>): Observable<T>{
-    return undefined;
-  }
-
   loadingOn(){
     this.loadingSubject.next(true);
   }
 
-  
   loadingOff(){
     this.loadingSubject.next(false);
   }
-
   
 }
