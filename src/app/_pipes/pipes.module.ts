2b9ecb954a36752778399ebdf192fa2c74d1f450
Ali M <alimousavi-01@outlook.com>
2022-01-05 12:29:40 +0330

    Created PipesModule

---
+++ src/app/_pipes/pipes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThousandSeperatorPipe } from './thousand-seperator/thousand-seperator.pipe';
+
+@NgModule({
+  declarations: [
+    ThousandSeperatorPipe
+  ],
+  imports: [CommonModule],
+  exports: [
+    ThousandSeperatorPipe
+  ],
+  schemas: []
+})
+export class PipesModule { }
