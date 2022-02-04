6f503e92572c7a854ed25f7420e9c8944a4ac839
Ali M <alimousavi-01@outlook.com>
2022-02-04 13:20:46 +0330

    fully implemented ArticleCategoryService

--- src/app/_services/blog/article-category/article-category.service.ts
+++ src/app/_services/blog/article-category/article-category.service.ts
@@ -6,7 +6,7 @@ import { tap, catchError, map } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { LoadingService } from '@loading';
-import { EditArticleCategoryModel, FilterArticleCategoryModel } from '@app_models/blog/article-category/_index';
+import { CreateArticleCategoryModel, EditArticleCategoryModel, FilterArticleCategoryModel } from '@app_models/blog/article-category/_index';
 
 @Injectable({
   providedIn: 'platform'
@@ -51,6 +51,99 @@ export class ArticleCategoryService {
     (`${environment.blogBaseApiUrl}/article-category/${id}`)
     .pipe(
       tap(() => this.loading.loadingOff()),
+      catchError((error: HttpErrorResponse) => {
+
+        this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+        this.loading.loadingOff();
+
+        return throwError(error);
+      })
+    );
+  }
+
+  createArticleCategory(createData: CreateArticleCategoryModel):Observable<IResponse<any>> {
+
+    this.loading.loadingOn();
+
+    const formData = new FormData();
+    
+    formData.append('title', createData.title);
+    formData.append('description', createData.description);
+    formData.append('imageFile', createData.imageFile, createData.imageFile.name);
+    formData.append('imageAlt', createData.imageAlt);
+    formData.append('imageTitle', createData.imageTitle);
+    formData.append('metaKeywords', createData.metaKeywords);
+    formData.append('metaDescription', createData.metaDescription);
+    
+    return this.http.post<IResponse<any>>
+    (`${environment.blogBaseApiUrl}/article-category/create`, formData)
+    .pipe(
+      tap((res: IResponse<any>) => {
+
+        this.toastr.success(res.message, 'موفقیت', { timeOut: 1500 });
+        this.loading.loadingOff();
+
+      }),
+      catchError((error: HttpErrorResponse) => {
+
+        this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+        this.loading.loadingOff();
+
+        return throwError(error);
+      })
+    );
+  }
+
+  editArticleCategory(editData: EditArticleCategoryModel):Observable<IResponse<any>> {
+    
+    this.loading.loadingOn();
+
+    const formData = new FormData();
+
+    formData.append('id', editData.id.toString());
+    formData.append('title', editData.title);
+    formData.append('description', editData.description);
+
+    if(editData.imageFileUploaded){
+      formData.append('imageFile', editData.imageFile, editData.imageFile.name);
+    }
+
+    formData.append('imageAlt', editData.imageAlt);
+    formData.append('imageTitle', editData.imageTitle);
+    formData.append('metaKeywords', editData.metaKeywords);
+    formData.append('metaDescription', editData.metaDescription);
+    
+    return this.http.put<IResponse<any>>
+    (`${environment.blogBaseApiUrl}/article-category/edit`, formData)
+    .pipe(
+      tap((res: IResponse<any>) => {
+
+        this.toastr.success(res.message, 'موفقیت', { timeOut: 1500 });
+        this.loading.loadingOff();
+
+      }),
+      catchError((error: HttpErrorResponse) => {
+
+        this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+        this.loading.loadingOff();
+
+        return throwError(error);
+      })
+    );
+  }
+
+  deleteArticleCategory(ArticleCategoryId: number):Observable<IResponse<any>> {
+    this.loading.loadingOn();
+
+    return this.http.delete<IResponse<any>>
+    (`${environment.blogBaseApiUrl}/article-category/delete/${ArticleCategoryId}`)
+    .pipe(
+      tap((res: IResponse<any>) => {
+
+        this.toastr.success(res.message, 'موفقیت', { timeOut: 1500 });
+        this.loading.loadingOff();
+
+      }),
       catchError((error: HttpErrorResponse) => {
 
         this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
