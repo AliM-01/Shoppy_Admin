284256d368b2945b75cb3ec02c69547332b727ed
Ali M <alimousavi-01@outlook.com>
2022-03-01 01:08:43 +0330

    Created Auth Models

---
+++ src/app/_models/auth/_index.ts
@@ -0,0 +1,4 @@
+export * from './account';
+export * from './login-response';
+export * from './login-request';
+export * from './revoke-refresh-token-request';
---
+++ src/app/_models/auth/account.ts
@@ -0,0 +1,8 @@
+export class AccountModel {
+    id: string;
+    role: string;
+    fullName: string;
+    email: string;
+    avatarPath: string;
+    registerDate: string;
+}
