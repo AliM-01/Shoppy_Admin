e6eeda302be3c28d86245fea08768275df0cdad7
Ali M <alimousavi-01@outlook.com>
2022-03-16 12:54:44 +0330

    Created FilterAccountModel

---
+++ src/app/_models/account/filter-account.ts
@@ -0,0 +1,33 @@
+import { AccountModel } from './account';
+import { PagingDataSortIdOrder, IPaging, PagingDataSortCreationDateOrder } from '../_common/IPaging';
+
+
+export class FilterAccountModel implements IPaging {
+
+    pageId: number;
+    allPagesCount: number;
+    takePage: number;
+    sortCreationDateOrder: PagingDataSortCreationDateOrder;
+    sortIdOrder: PagingDataSortIdOrder;
+    fullName: string;
+    email: string;
+    accounts: AccountModel[];
+
+   constructor(
+        fullName: string,
+        email: string,
+        accounts: AccountModel[],
+        pageId: number,
+        takePage: number,
+        sortCreationDateOrder: PagingDataSortCreationDateOrder,
+        sortIdOrder: PagingDataSortIdOrder
+        ){
+        this.fullName = fullName;
+        this.email = email;
+        this.accounts = accounts;
+        this.pageId = pageId;
+        this.takePage = takePage;
+        this.sortCreationDateOrder = sortCreationDateOrder;
+        this.sortIdOrder = sortIdOrder;
+    }
+}
