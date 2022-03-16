1f868fc764ee64175af9928c257edbf75858f98b
Ali M <alimousavi-01@outlook.com>
2022-03-16 13:25:27 +0330

    AccountRoutingModule

--- src/app/pages/account/account.routing.module.ts
+++ src/app/pages/account/account.routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { FilterAccountPage } from "./filter-account/filter-account.page";
 
 const routes: Routes = [
+  { path:'', component:FilterAccountPage },
 ];
 
 @NgModule({
