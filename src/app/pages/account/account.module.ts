71cf754e5c388655cfaedef296101e8d7cd34aca
Ali M <alimousavi-01@outlook.com>
2022-03-16 13:08:58 +0330

    Created AccountModule

---
+++ src/app/pages/account/account.module.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '@app_components/components.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DirectivesModule } from '@app_directives/directives.module';
+import { PipesModule } from '@app_pipes/pipes.module';
+import { AppMaterialModule } from '@app/app-material.module';
+import { AccountService } from '@app_services/account/account.service';
+import { AccountRoutingModule } from './account.routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    AccountRoutingModule,
+    ComponentsModule,
+    FormsModule,
+    AppMaterialModule,
+    DirectivesModule,
+    PipesModule
+  ],
+  exports: [],
+  schemas: [],
+  providers: [AccountService]
+})
+export class AccountModule { }
