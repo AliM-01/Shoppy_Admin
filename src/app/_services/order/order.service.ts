e875bfebaddd7cb0f0783b50b19a7ab5958d321b
Ali M <alimousavi-01@outlook.com>
2022-03-22 20:51:46 +0430

    Created OrderService

---
+++ src/app/_services/order/order.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IResponse } from '@app_models/_common/IResponse';
+import { environment } from '@environments/environment';
+import { tap, catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { LoadingService } from '@loading';
+
+@Injectable({
+  providedIn: 'platform'
+})
+export class OrderService {
+  constructor(
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private loading: LoadingService,
+  ) { }
+
+
+
+}
