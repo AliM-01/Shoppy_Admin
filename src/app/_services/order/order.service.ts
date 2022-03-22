7ced49b2f136b0e4f9a0e2141450dc5f4b7771b4
Ali M <alimousavi-01@outlook.com>
2022-03-22 21:12:44 +0430

    Implemented filterOrder service

--- src/app/_services/order/order.service.ts
+++ src/app/_services/order/order.service.ts
@@ -6,6 +6,7 @@ import { tap, catchError, map } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { LoadingService } from '@loading';
+import { FilterOrderModel } from '@app_models/order/filter-inventory';
 
 @Injectable({
   providedIn: 'platform'
@@ -18,5 +19,32 @@ export class OrderService {
   ) { }
 
 
+  filterOrder(filter: FilterOrderModel): Observable<IResponse<FilterOrderModel>> {
+
+    this.loading.loadingOn()
+
+    let params = new HttpParams();
+
+    if(filter.userNames){
+      params.set('UserNames', filter.userNames)
+    }
+
+    if(filter.paymentState){
+      params.set('PaymentState', filter.paymentState)
+    }
+
+    return this.http.get<IResponse<FilterOrderModel>>
+      (`${environment.orderBaseApiUrl}/filter`, { params })
+      .pipe(
+        tap(() => this.loading.loadingOff()),
+        catchError((error: HttpErrorResponse) => {
+
+          this.toastr.error(error.error.message, 'خطا', { timeOut: 2500 });
+          this.loading.loadingOff();
+
+          return throwError(error);
+        })
+      );
+  }
 
 }
